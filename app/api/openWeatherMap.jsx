a00b2c12604a3d0fd78b83003def15d3770050ad
mukesh92 <chouhan.mukesh17@gmail.com>
2017-06-01 21:38:45 +0530

    cors

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,11 +1,18 @@
-import axios from "axios";
+import reqwest from "reqwest";
 const OPEN_WEATHER_URL = "http://samples.openweathermap.org/data/2.5/weather?appid=bf8dc15b12c4d7453214f3a5f8175983";
+
 
 module.exports= {
 	getTemp : function(location){
 	var encodeLocation = encodeURIComponent(location);
 	var reqUrl = `${OPEN_WEATHER_URL}&q=${encodeLocation}`;
-	return axios.get(reqUrl).then((res)=>{
+	return reqwest({
+								url : reqUrl,
+								type: 'json',
+  							method: 'GET',
+								contentType: 'application/json',
+								crossOrigin: true
+							}).then((res)=>{
 							if(res.cod && res.message){
 								throw new Error(res.message);
 							}else{
